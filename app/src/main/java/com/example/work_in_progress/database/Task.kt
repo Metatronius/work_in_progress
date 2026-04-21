aa4ba5fce0aedfb4362ec4ff2f933443721b3a59
vprljic <vojislavmprljic@gmail.com>
2026-04-21 18:38:55 -0500

    Created DB classes

---
+++ app/src/main/java/com/example/work_in_progress/database/Task.kt
@@ -0,0 +1,9 @@
+package com.example.work_in_progress.database
+import androidx.room.*
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val isCompleted: Boolean = false
+)
---
+++ app/src/main/java/com/example/work_in_progress/database/TaskDatabase.kt
@@ -0,0 +1,25 @@
+package com.example.work_in_progress.database
+import android.content.Context
+import androidx.room.*
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase: RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}

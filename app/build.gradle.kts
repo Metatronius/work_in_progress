2e4b2782b87e77538d304de6184cd41c8fe00035
vprljic <vojislavmprljic@gmail.com>
2026-04-21 18:13:52 -0500

    Added KSP to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.example.work_in_progress"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.work_in_progress"
         minSdk = 31
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
